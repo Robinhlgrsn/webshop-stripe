24b1596757b5dcc9e7be4184084549cb88f52142
Anton Månsson <anton.mansson@mediainstitutet.se>
2021-09-22 14:56:23 +0200

    count totalprice

--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -1,50 +1,32 @@
 import React, { useState } from "react";
 import CartItem from "./CartItem";
-const testData = [
-  {
-    img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
-    name: "Shoe",
-    quantity: "2",
-    price: "500",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
-    name: "Hat",
-    quantity: "1",
-    price: "500",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
-    name: "Cap",
-    quantity: "2",
-    price: "500",
-  },
-];
+import products from "../../data/products";
+import Button from "../UI/Button";
 export default function Cart() {
-  const [totalAmount, setTotalAmount] = useState(0);
-
+  const [totalPriceInCart, settotalPriceInCart] = useState(0);
+  /* Function som räknar det totala priset för produkterna i varukorgen */
   const calcTotalPrice = () => {
     let price;
     let quantity;
     let totalPriceItem;
 
     let array = [];
-    testData.forEach((item) => {
+    products.forEach((item) => {
       price = item.price;
       quantity = item.quantity;
       totalPriceItem = quantity * price;
       array.push(totalPriceItem);
     });
     let total = array.reduce((a, b) => a + b);
-    setTotalAmount(total);
+    settotalPriceInCart(total);
     console.log(total);
   };
   return (
-    <div className="container mx-auto bg-green-600 flex flex-col">
-      {testData.map((item) => (
+    <div className="container mx-auto flex flex-col">
+      {products.map((item) => (
         <div key={item.name}>
           <CartItem
-            img={item.img}
+            imgUrl={item.imgUrl}
             name={item.name}
             quantity={item.quantity}
             price={item.price}
@@ -52,13 +34,11 @@ export default function Cart() {
         </div>
       ))}
 
-      <div className=" bg-green-600 flex justify-between text-4xl">
-        <div className=" bg-green-600">Total price</div>
-        <div className=" bg-red-600 px-8">{totalAmount}:-</div>
+      <div className=" flex justify-between text-4xl">
+        <div>Total price</div>
+        <div className="px-8">{totalPriceInCart}:-</div>
       </div>
-      <button onClick={calcTotalPrice} className="self-center">
-        CheckOut
-      </button>
+      <Button>CheckOut</Button>
     </div>
   );
 }
--- client/src/components/cart/CartItem.js
+++ client/src/components/cart/CartItem.js
@@ -3,10 +3,17 @@ import React from "react";
 export default function CartItem(props) {
   return (
     <div className="container mx-auto flex justify-between text-4xl items-center">
-      <div className="flex  items-center h-32">
-        <img src={props.img} alt="" className="object-contain h-full w-full" />
+      <div className="flex  items-center ">
+        <div className="h-32 w-32">
+          <img
+            src={props.imgUrl}
+            alt=""
+            className="object-cover h-full w-full"
+          />
+        </div>
         <div className="px-8 ">{props.name}</div>
       </div>
+
       <div className="flex  h-full">
         <button>-</button>
         <div className="px-4">{props.quantity}</div>
